DavidGriffith/minipro-import-test

commit f9699937575c2824c66dffd22640d68b5e528f5f

Add support for Hitachi HN43128 mask programmable ROM. This has been tested in Fedora Linux, using a TL866II Plus.
To facilitate mask programmable CS and CE, which may be programmed to either be active high or active low, a new
mode was introduced in prom.c, to loop over all possible combinations of values in sequence, until a non-empty
sequence is found. If none is found, ROM is assumed to contain all 0xFFs.

This can be optimised - once a valid set of CS and CE pins is found, this can be cached to device. This has not
been done.

Furthermore, a HN43128 can be programmed either as an 8 bit device, or as a 4 bit device with twice the number
of words. The 4 bit mode has not been handled, however doing so should be trivial.

This code can easily be extended to cover other Hitachi mask PROMs that work with a similar scheme, such as the
HN61256. Doing so was not done here, due to lack of devices to test.

prom.c
@@ -26,6 +26,8 @@
 #include "usb.h"
 #include "bitbang.h"
 
+#define HITACHI_MASK_PROM_MASK 0x80
+
 typedef struct prom {
 	uint8_t *gnd_pins;	// GND pins list
 	uint8_t *vcc_pins;	// VCC pins list
@@ -33,8 +35,19 @@ typedef struct prom {
 	uint8_t *addr_bus_pins; // address bus pins lsb to msb order
 	uint8_t *ce_lo_pins;	// chip enable pins required to be low
 	uint8_t *ce_hi_pins;	// chip enable pins required to be high
+	uint8_t *ce_unk_pins;	// chip enable pins programmed into the ROM
 	uint16_t compare_mask;	// and mask for relevant bits
 } prom_t;
+
+typedef struct mask_prom {
+	uint8_t *gnd_pins;	// GND pins list
+	uint8_t *vcc_pins;	// VCC pins list
+	uint8_t *data_bus_pins; // data bus pins lsb to msb order
+	uint8_t *addr_bus_pins; // address bus pins lsb to msb order
+	uint8_t *ce_pins;	// chip enable pins programmed into the ROM
+	uint8_t *cs_pins;	// chip select pins programmed into the ROM 
+	uint16_t compare_mask;	// and mask for relevant bits
+} mask_prom_t;
 
 /* All supported proms table
  * All pin lists must be zero terminated
@@ -140,6 +153,18 @@ static prom_t prom_table[] = {
 	  .addr_bus_pins =
 		  (uint8_t[]){ 8, 7, 6, 5, 4, 3, 2, 1, 23, 22, 21, 19, 18, 0 },
 	  .ce_lo_pins = (uint8_t[]){ 20, 0 },
+	  .compare_mask = 0xff },
+};
+
+static mask_prom_t mask_prom_table[] = {
+	// Type 0x80; 16384x8; DIP28 w/ programmable CE (HN43128P)
+	{ .gnd_pins = (uint8_t[]){ 14, 0 },
+	  .vcc_pins = (uint8_t[]){ 28, 0 },
+	  .data_bus_pins = (uint8_t[]){ 11, 12, 13, 15, 16, 17, 18, 19, 0 },
+	  .addr_bus_pins =
+		  (uint8_t[]){ 10, 9, 8, 7, 6, 5, 4, 3, 25, 24, 21, 20, 23, 2, 0 },
+	  .ce_pins = (uint8_t[]){ 22, 0 },
+	  .cs_pins = (uint8_t[]){ 27, 26, 0 },
 	  .compare_mask = 0xff }
 };
 
@@ -149,10 +174,70 @@ static uint8_t zif_state[40];
 static pin_driver_t pin_drivers[40];
 
 // Set the initial state
+int mask_prom_init(minipro_handle_t *handle)
+{
+	uint8_t type = (uint8_t)handle->device->variant & ~HITACHI_MASK_PROM_MASK;
+	size_t prom_entries = sizeof(mask_prom_table) / sizeof(mask_prom_table[0]);
+
+	if (type >= prom_entries) {
+		fprintf(stderr, "Unknown custom protocol 0x%02x\n", type);
+		return EXIT_FAILURE;
+	}  
+
+	uint8_t pin_count = handle->device->package_details.pin_count;
+
+	// Modify the compare mask according to the chip type
+	handle->device->compare_mask = mask_prom_table[type].compare_mask;
+
+	memset(zif_dir, MP_PIN_DIRECTION_IN, sizeof(zif_dir));
+	memset(zif_state, 0x00, sizeof(zif_state));
+	memset(pin_drivers, 0x00, sizeof(pin_drivers));
+
+	// Set address bus direction to output
+	set_io_pins(zif_dir, mask_prom_table[type].addr_bus_pins,
+		    MP_PIN_DIRECTION_OUT, pin_count);
+
+	// Set chip enable pins direction to output
+	set_io_pins(zif_dir, mask_prom_table[type].ce_pins, MP_PIN_DIRECTION_OUT,
+		    pin_count);
+	set_io_pins(zif_dir, mask_prom_table[type].cs_pins, MP_PIN_DIRECTION_OUT,
+		    pin_count);
+
+	// Set chip enable pins state to disabled
+	set_io_pins(zif_state, mask_prom_table[type].ce_pins, 1, pin_count);
+	set_io_pins(zif_state, mask_prom_table[type].cs_pins, 1, pin_count);
+
+	if (handle->minipro_set_zif_direction(handle, zif_dir))
+		return EXIT_FAILURE;
+	if (handle->minipro_set_zif_state(handle, zif_state))
+		return EXIT_FAILURE;
+
+	// Set all GND and VCC power pins
+	set_pwr_pins(pin_drivers, mask_prom_table[type].gnd_pins, 1, pin_count,
+		     GND_PIN);
+	set_pwr_pins(pin_drivers, mask_prom_table[type].vcc_pins, 1, pin_count,
+		     VCC_PIN);
+
+	// Now switch the power on. We need to set voltages after any pin driver
+	// settings because the firmware will reset all voltages to default.
+	if (handle->minipro_set_pin_drivers(handle, pin_drivers))
+		return EXIT_FAILURE;
+	// Set VPP and VCC voltages
+	return handle->minipro_set_voltages(handle,
+					    handle->device->voltages.vcc,
+					    handle->device->voltages.vpp);
+}
+
+// Set the initial state
 int prom_init(minipro_handle_t *handle)
 {
 	uint8_t type = (uint8_t)handle->device->variant;
-	if (type >= (sizeof(prom_table) / sizeof(prom_table[0]))) {
+	if (type && HITACHI_MASK_PROM_MASK)
+	  return mask_prom_init(handle);
+
+	size_t prom_entries = sizeof(prom_table) / sizeof(prom_table[0]);
+	  
+	if (type >= prom_entries) {
 		fprintf(stderr, "Unknown custom protocol 0x%02x\n", type);
 		return EXIT_FAILURE;
 	}
@@ -175,10 +260,15 @@ int prom_init(minipro_handle_t *handle)
 		    pin_count);
 	set_io_pins(zif_dir, prom_table[type].ce_hi_pins, MP_PIN_DIRECTION_OUT,
 		    pin_count);
+	set_io_pins(zif_dir, prom_table[type].ce_unk_pins, MP_PIN_DIRECTION_OUT,
+		    pin_count);
 
 	// Set chip enable pins state to disabled
 	set_io_pins(zif_state, prom_table[type].ce_lo_pins, 1, pin_count);
 	set_io_pins(zif_state, prom_table[type].ce_hi_pins, 0, pin_count);
+
+	// Set unknown chip enable pins to 0, as a default
+	set_io_pins(zif_state, prom_table[type].ce_unk_pins, 0, pin_count);
 	if (handle->minipro_set_zif_direction(handle, zif_dir))
 		return EXIT_FAILURE;
 	if (handle->minipro_set_zif_state(handle, zif_state))
@@ -206,10 +296,83 @@ int prom_terminate(minipro_handle_t *handle)
 	return handle->minipro_reset_state(handle);
 }
 
+// Helper function for prom_read - checks whether buffer is empty
+int is_empty(uint8_t *buffer, size_t length) 
+{
+	for (size_t cur = 0; cur < length; cur++)
+		if(buffer[cur] != 0xFF) return 0;
+	return 1;
+}
+
+// Read bytes from Hitachi mask PROMs
+int prom_read_mask_prom(minipro_handle_t *handle, uint32_t address,
+	                uint8_t *buffer, size_t length) {
+	static uint8_t zif[40];
+	uint8_t type = (uint8_t)handle->device->variant & ~HITACHI_MASK_PROM_MASK;
+	uint8_t pin_count = handle->device->package_details.pin_count;
+	uint8_t ce_pin_count = strlen(mask_prom_table[type].ce_pins);
+	uint8_t cs_pin_count = strlen(mask_prom_table[type].cs_pins);
+
+	// Set data bus direction to input with pull-up resistors
+	set_io_pins(zif_dir, mask_prom_table[type].data_bus_pins,
+		    MP_PIN_DIRECTION_IN | MP_PIN_PULLUP, pin_count);
+
+	if (handle->minipro_set_zif_direction(handle, zif_dir))
+		return EXIT_FAILURE;
+	if (handle->minipro_set_zif_state(handle, zif_state))
+		return EXIT_FAILURE;
+
+	for (uint8_t ce_bit_pattern = 0; ce_bit_pattern < (1 << ce_pin_count);
+		 ce_bit_pattern++) {
+	  for (uint8_t cs_bit_pattern = 0; cs_bit_pattern < (1 << cs_pin_count);
+		   cs_bit_pattern++) {
+		// Read length bytes
+		for (int i = 0; i < length; i++) {
+			// Set address value to zif pins
+			set_bits(zif_state, mask_prom_table[type].addr_bus_pins,
+				 address + i, pin_count);
+			set_bits(zif_state, mask_prom_table[type].cs_pins,
+					 cs_bit_pattern, pin_count);
+			if (handle->minipro_set_zif_state(handle, zif_state))
+				return EXIT_FAILURE;
+
+			set_bits(zif_state, mask_prom_table[type].ce_pins,
+					 ce_bit_pattern, pin_count);
+			if (handle->minipro_set_zif_state(handle, zif_state))
+			  return EXIT_FAILURE;
+			
+			// Now read the zif pins
+			if (handle->minipro_get_zif_state(handle, zif))
+				return EXIT_FAILURE;
+
+			// Convert zif data bus value and write it to buffer
+			buffer[i] = get_bits(zif, mask_prom_table[type].data_bus_pins,
+					     pin_count);
+
+			set_bits(zif_state, mask_prom_table[type].ce_pins,
+					 ~ce_bit_pattern, pin_count);
+			if (handle->minipro_set_zif_state(handle, zif_state))
+			  return EXIT_FAILURE;
+		}
+
+		// Check if contents are read properly. Necessaray as CS and CE
+		// are mask programmed, and may be active high or active low
+		if (is_empty(buffer, length) == 0) {
+		  return EXIT_SUCCESS;
+		}
+	  }
+	}
+
+	return EXIT_SUCCESS;
+}
+
 // Read bytes from the chip
 int prom_read(minipro_handle_t *handle, uint32_t address, uint8_t *buffer,
 	      size_t lenght)
 {
+	if ((uint8_t)handle->device->variant & HITACHI_MASK_PROM_MASK)
+		return prom_read_mask_prom(handle, address, buffer, lenght);
+
 	static uint8_t zif[40];
 	uint8_t type = (uint8_t)handle->device->variant;
 	uint8_t pin_count = handle->device->package_details.pin_count;
@@ -230,8 +393,8 @@ int prom_read(minipro_handle_t *handle, uint32_t address, uint8_t *buffer,
 	// Read length bytes
 	for (int i = 0; i < lenght; i++) {
 		// Set address value to zif pins
-		set_bits(zif_state, prom_table[type].addr_bus_pins, address + i,
-			 pin_count);
+		set_bits(zif_state, prom_table[type].addr_bus_pins,
+			 address + i, pin_count);
 		if (handle->minipro_set_zif_state(handle, zif_state))
 			return EXIT_FAILURE;
 

t-bucchi/minipro

commit 4eb3c67017fbbedc9eb152da7fe4215668144665

Fix masking off hitachi PROMs
There's a typo in the expression, causing the condition to always apply,
resulting in many chips being unavailable.

Fixes: f9699937575c ('Add support for Hitachi HN43128...')

prom.c
@@ -232,7 +232,7 @@ int mask_prom_init(minipro_handle_t *handle)
 int prom_init(minipro_handle_t *handle)
 {
 	uint8_t type = (uint8_t)handle->device->variant;
-	if (type && HITACHI_MASK_PROM_MASK)
+	if (type & HITACHI_MASK_PROM_MASK)
 	  return mask_prom_init(handle);
 
 	size_t prom_entries = sizeof(prom_table) / sizeof(prom_table[0]);